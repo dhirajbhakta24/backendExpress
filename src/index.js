fd8c5176c778a3b7a4503e126ba42c9b7c4bfbc6
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2024-04-24 13:08:42 +0530

    added express middleware in  backendExpress

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const{ PORT} = require('./config/server.config');
 
+// when we call the function express we create a new express server object
 
-const app = express();
+const app = express();  //http server object 
 
-app.get('/home', (req,res) => {
+//express middleware 
+
+function m1(req , res ,next){
+    console.log('Inside Middleware m1');
+    next();
+
+}
+function m2(req , res , next){
+    console.log('Inside Middleware m2');
+    next();
+}
+
+
+app.get('/home', m1 , m2 , (req,res) => {
+    //everytime someone calls the /home route , this callback will be called
+    
     return res.json({msg :'Welcome home'});
-})
+
+}) 
+
 
 app.listen(PORT, ()=>{
     console.log(`Started server at port: ${PORT} `);
